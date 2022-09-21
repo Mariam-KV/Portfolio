4edc8815e5e58c6d66cf944c07e13af8b67878dd
Mashawabupr <marikanik1999@gmail.com>
2022-09-21 21:11:59 +0300

    added projects page

--- script.js
+++ script.js
@@ -46,3 +46,24 @@ document.addEventListener("keydown", function (e) {
   }
   //same e.key === "ArrowRight" && moveLeft()
 });
+
+//projects
+let btn1 = document.querySelector(".button1");
+let btn2 = document.querySelector(".button2");
+
+let buttons = document
+  .querySelector(".projects__main")
+  .addEventListener("click", function (el) {
+    if (el.target.classList.contains("loans")) {
+      btn1.classList.remove("hidecontent");
+      adding(btn2);
+    }
+    if (el.target.classList.contains("close")) {
+      adding(btn1);
+      btn2.classList.remove("hidecontent");
+    }
+  });
+
+function adding(first) {
+  first.classList.add("hidecontent");
+}
