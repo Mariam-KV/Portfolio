ac6ac0b8c61e8ddbbb05a2b488813404e73b5fde
Mashawabupr <marikanik1999@gmail.com>
2022-09-22 17:03:08 +0300

    deleted

--- script.js
+++ script.js
@@ -46,3 +46,23 @@ document.addEventListener("keydown", function (e) {
   }
   //same e.key === "ArrowRight" && moveLeft()
 });
+//projects
+let first = document.querySelector(".first");
+let second = document.querySelector(".second");
+
+let buttons = document
+  .querySelector(".projects__buttons")
+  .addEventListener("click", function (el) {
+    if (el.target.classList.contains("button1")) {
+      first.classList.remove("hidecontent");
+      adding(second);
+    }
+    if (el.target.classList.contains("button2")) {
+      adding(first);
+      second.classList.remove("hidecontent");
+    }
+  });
+
+function adding(first) {
+  first.classList.add("hidecontent");
+}
