0791c36018975705998b7301d846f43059f08b93
Mashawabupr <marikanik1999@gmail.com>
2022-10-10 21:40:39 +0300

    debugging style

--- JS/changeTheme.js
+++ JS/changeTheme.js
@@ -3,6 +3,8 @@ let moon = document.getElementById("moon");
 let purple = document.querySelectorAll(".purple");
 let btn = document.querySelector(".btn");
 let sticky = document.querySelector(".sticky");
+let projectTitle = document.querySelectorAll(".description");
+let projectTitle2 = document.querySelectorAll(".projectDescription");
 //let menu = document.querySelector(".make-menu");
 
 export let changeTheme = function () {
@@ -23,6 +25,12 @@ export let changeTheme = function () {
       el.style.fill = "#FFFFFF";
       el.style.borderColor = "#FFFFFF";
     });
+    projectTitle.forEach((el) => {
+      el.style.color = "#FFFFFF";
+    });
+    projectTitle2.forEach((el) => {
+      el.style.color = "#FFFFFF";
+    });
   });
   moon.addEventListener("click", () => {
     moon.style.display = "none";
@@ -32,6 +40,12 @@ export let changeTheme = function () {
     btn.style.backgroundColor = "#4D47B8";
     btn.style.color = "#FFFFFF";
 
+    projectTitle.forEach((el) => {
+      el.style.color = "#4D47B8";
+    });
+    projectTitle2.forEach((el) => {
+      el.style.color = "#4D47B8";
+    });
     purple.forEach((el) => {
       el.style.color = "#4D47B8";
       el.style.fill = "#4D47B8";
